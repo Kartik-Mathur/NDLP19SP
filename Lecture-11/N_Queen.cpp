b24d5285725fada840062549b02c93294414c6d8
Kartik Mathur <kartikmathur@Kartiks-MacBook-Air.local>
2019-03-03 16:02:10 +0530

    Lecture-11

---
+++ Lecture-11/N_Queen.cpp
@@ -0,0 +1,80 @@
+// N_Queen
+#include <iostream>
+using namespace std;
+
+bool CanPlaceQueenHere(int board[][10],int i,int j,int n){
+
+	for(int k=0;k<n;k++){
+		if(board[i][k]||board[k][j]){
+			return false;
+		}
+	}
+
+	// Left diagnol
+	int k=i;
+	int l=j;
+
+	while(k>=0 && l>=0){
+		if(board[k][l]){
+			return false;
+		}
+		k--;
+		l--;
+	}
+
+	// Right Diagnol
+	k=i;l=j;
+	while(k>=0 && l<n){
+		if(board[k][l]){
+			return false;
+		}
+		k--;
+		l++;
+	}
+
+	return true;
+}
+
+
+bool NQueen(int board[][10],int n,int i){
+	// base case
+	if(i==n){
+		for(int k=0;k<n;k++){
+			for(int l=0;l<n;l++){
+				cout<<board[k][l]<<" ";
+			}
+			cout<<endl;
+		}
+		cout<<endl;
+		return false;
+	}
+
+	// Recursive case
+	for(int j=0;j<n;j++){
+		if(CanPlaceQueenHere(board,i,j,n)){
+			board[i][j]=1;
+			bool KyaBaakiBaatBani=NQueen(board,n,i+1);
+			if(KyaBaakiBaatBani){
+				return true;
+			}
+			board[i][j]=0;
+		}
+	}
+	return false;
+}
+
+
+
+int main(){
+	int board[10][10]={0};
+	int n;
+	cin>>n;
+
+	NQueen(board,n,0);
+
+	cout<<endl;
+
+
+
+	return 0;
+}
